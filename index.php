5f8f61b0bebd5c98e4c479ff334ea68fc7bff85e
Basant Mallick <87462488+webcode005@users.noreply.github.com>
2022-05-18 13:13:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,64 +14,11 @@ if(isset($_POST['submit']))
       mysqli_query($conn, $sql);
     }
 
-    //echo "<p>faq has been added.</p>";
-
     echo "<script>alert('faq Added Successfully');window.location='faq.php'</script>";
-
-     
-    // if ($conn->query($sql)== TRUE) 
-    // {
-        
-
-    //     echo "<script>alert('faq Added Successfully');window.location='faq.php'</script>";
-
-
-    // } 
-    // else 
-    // {
-    //     //echo "<script>alert('Failed to Insert Data');window.location='faq.php'</script>";
-
-
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    // }
-    
-
-
 }
-// Check Availability End
 
 ?>
 
-
-<!-- end sidebar menu -->
-<!-- start page content -->
-<div class="page-content-wrapper">
-    <div class="page-content">
-        <div class="page-bar">
-            <div class="page-title-breadcrumb">
-                <div class=" pull-left">
-                    <div class="page-title">FAQs</div>
-                </div>
-                <ol class="breadcrumb page-breadcrumb pull-right">
-                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="index.php">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
-                    </li>
-
-                    <li class="active">FAQs</li>
-                </ol>
-            </div>
-        </div>
-
-        <div class="row">
-
-                <div class="col-md-12">
-                <div class="card card-topline-red">
-                    <div class="card-head">
-                        <header>Add faq Content For Product</header>
-
-                    </div>
-                    <div class="card-body ">
-                        <div class="row p-b-20">
-                            <div class="col-md-12 col-sm-12 col-12">
 
                                  <form method="post" enctype="multipart/form-data">
 
@@ -143,39 +90,9 @@ if(isset($_POST['submit']))
                                       <button type="submit" name="submit" class="btn btn-primary mt-3">Submit</button>
                                     </form>
 
-                            </div>
-
-                        </div>
-</div>
-</div>
-</div>
-
-
-         
-            </div>
-        </div>
-
-    </div>
-</div>
+                         
 <!-- end page content -->
 
-<!-- start footer -->
-
-<?php include'include/footer.php';?>
-
-
-<style type="text/css">
-  table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  table tr td,
-  table tr th {
-    border: 1px solid black;
-    padding: 15px;
-        text-align: center;
-  }
-</style>
 
 <script type="text/javascript">
         // add row
